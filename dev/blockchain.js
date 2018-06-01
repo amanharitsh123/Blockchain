a662227ebce4d8008ee29f0f4e86d1a2b92d0764
amanharitsh123 <amanharitsh123@gmail.com>
2018-06-01 00:56:06 -0700

    API Foundation Built

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -3,6 +3,7 @@ const sha256 = require('sha256');
 function Blockchain() {
 	this.chain = [];
 	this.pendingTransactions = [];
+	this.createNewBlock(100, '0','0');
 }
 
 Blockchain.prototype.createNewBlock = function(nounce, previousBlockHash, hash) {
@@ -27,7 +28,6 @@ Blockchain.prototype.getLastBlock = function() {
 }
 
 
-
 Blockchain.prototype.createNewTransaction = function(amount,sender,recipient) {
 	const newTransaction = {
 		amount: amount,
@@ -41,6 +41,18 @@ Blockchain.prototype.hashBlock = function(previousBlockHash,currentBlockData,nou
 	const dataAsString = previousBlockHash + nounce.toString() + JSON.stringify(currentBlockData);
 	const hash = sha256(dataAsString);
 	return hash;
+}
+
+Blockchain.prototype.proofOfWork = function(previousBlockHash,currentBlockData) {
+	let nounce = 0;
+	let hash = this.hashBlock(previousBlockHash,currentBlockData,nounce);
+	//let and const are block scope vars . const variable are immutable 
+	while (hash.substring(0,4) !== '0000') {
+		nounce++;
+		hash = this.hashBlock(previousBlockHash,currentBlockData,nounce);
+
+	}
+	return nounce;
 }
 
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -1,13 +1,25 @@
 const Blockchain = require('./blockchain')
 const bitcoin =  new Blockchain();
-const previousBlockHash = 'NDDBDHBVIBOKJOKJKNN'
-const currentBlockData = [
-	{
-		amount: 10,
-		sender :'csdacdscdscsd',
-		recipient :'NCKNANOOPMLPAWJDIAMLM'
-	}
+console.log(bitcoin);
+// const previousBlockHash = 'NDDBDHBVIBOKJOKJKNN'
+// const currentBlockData = [
+// 	{
+// 		amount: 10,
+// 		sender :'csdacdscdscsd',
+// 		recipient :'NCKNANOOPMLPAWJDIAMLM'
+// 	},
+// 	{
+// 		amount: 94594,
+// 		sender :'KDKCNEWINI4439U4',
+// 		recipient :'KLPJCSKADNIOWF93R4'
+// 	},
+// 	{
+// 		amount: 109,
+// 		sender :'VCVNJSDNVAKDNCSAKVBA',
+// 		recipient :'OIEGJREIFJEWIGEWVBO2'
+// 	}
 
-];
-const nounce = 100;
-console.log(bitcoin.hashBlock(previousBlockHash,currentBlockData,nounce));
+
+// ];
+// console.log(bitcoin.proofOfWork(previousBlockHash,currentBlockData));
+// console.log(bitcoin.hashBlock(previousBlockHash,currentBlockData,90496));
